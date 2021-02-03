b871d674de4a4afbad9c2e3f0c8d818b20101575
jeevan gangavarapu <jeevangangavarapu2@gmail.com>
2021-02-03 11:24:08 +0000

    adding condition to check prime for 1,0 and -ve numbers

--- isprime_function.py
+++ isprime_function.py
@@ -1,15 +1,16 @@
 def isprime(n):
-  for i in range(2,n):
-    if n%i==0:
-      print(n," is not a prime number")
-      break
-  else:
-    print(n," is a prime number")
-
-
-
-
-
+    if(n==1 or n==0 ):
+        print(n," is not a prime number")
+    elif(n<0):
+        print("please enter positive interger")
+    else:
+        
+      for i in range(2,n):
+        if (n%i==0):
+          print(n," is not a prime number")
+          break
+      else:
+        print(n," is a prime number")
 
 
 
